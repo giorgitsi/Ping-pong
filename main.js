f230d4becf0f03067e40cfe7c67fbc60e40c8538
giorgitsi <113306383+giorgitsi@users.noreply.github.com>
2025-05-04 11:00:00 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+let blueBtn = document.querySelector(".blue-btn")
+let redBtn = document.querySelector(".red-btn")
+let resetBtn = document.querySelector("reset-btn")
+let blueNum = document.querySelector(".blue-num")
+let redNum = document.querySelector(".red-num")
+
+let x = 0
+blueBtn.addEventListener("click", function () {
+    x = x + 1
+    blueNum.textContent = x
+})
+
+
+let y = 0
+redBtn.addEventListener("click", function () {
+    y = y + 1
+    redNum.textContent = y
+})
+
+resetBtn.addEventListener("click", function () {
+
+
+    redNum.textContent = "0"
+    blueNum.textContent = z
+
+})
